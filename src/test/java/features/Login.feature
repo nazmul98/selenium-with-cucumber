54fa26ce296a09354f811fdcc4774e60badc4473
nazmul98 <coder.nazmul@gmail.com>
2021-10-07 23:57:33 -0400

    handled parameter in gherkin file

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -2,8 +2,14 @@ Feature: Application Login
 
 Scenario: Home Page Default Login
 Given User is on NetBanking landing page
-When User login into application with username and password
+When User login into application with "John" and password "232323"
 Then Home page is displayed
-And All cards are also displayed
+And All cards are displayed "true"
+
+Scenario: Home Page Login
+Given User is on NetBanking landing page
+When User login into application with "Foo" and password "3434343"
+Then Home page is displayed
+And All cards are displayed "false"
 
 
--- src/test/java/stepDefination/StepDefination.java
+++ src/test/java/stepDefination/StepDefination.java
@@ -11,15 +11,14 @@ import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 public class StepDefination {
-
     @Given("^User is on NetBanking landing page$")
     public void user_is_on_netbanking_landing_page() throws Throwable {
         System.out.println("hello world 1");
     }
 
-    @When("^User login into application with username and password$")
-    public void user_login_into_application_with_username_and_password() throws Throwable {
-    	System.out.println("hello world 2");
+    @When("User login into application with {string} and password {string}")
+    public void user_login_into_application_with_and_password(String username, String password) {
+        System.out.println("Username: " + username + " Password: " + password);
     }
 
     @Then("^Home page is displayed$")
@@ -27,9 +26,8 @@ public class StepDefination {
     	System.out.println("hello world 3");
     }
 
-    @And("^All cards are also displayed$")
-    public void all_cards_are_also_displayed() throws Throwable {
-    	System.out.println("hello world 4");
+    @Then("All cards are displayed {string}")
+    public void all_cards_are_displayed(String status) {
+        System.out.print(status);
     }
-
 }
